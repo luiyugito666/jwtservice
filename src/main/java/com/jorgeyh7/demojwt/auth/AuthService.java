e03da532057ffa15377defa394b405acb2876cbf
jorge <yugito_is@hotmail.com>
2024-03-26 10:29:26 -0500

    fix jwt service

--- src/main/java/com/jorgeyh7/demojwt/auth/AuthService.java
+++ src/main/java/com/jorgeyh7/demojwt/auth/AuthService.java
@@ -22,7 +22,7 @@ public class AuthService {
    private final AuthenticationManager authenticationManager;
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-        UserDetails user=userRepository.findByUsername(request.getUsername()).orElseThrow();
+        User user=userRepository.findByUsername(request.getUsername()).orElseThrow();
         String token=jwtService.getToken(user);
         return AuthResponse.builder()
                 .token(token)
--- src/main/java/com/jorgeyh7/demojwt/jwt/JwtService.java
+++ src/main/java/com/jorgeyh7/demojwt/jwt/JwtService.java
@@ -1,14 +1,14 @@
 package com.jorgeyh7.demojwt.jwt;
 
+import com.jorgeyh7.demojwt.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.function.Function;
@@ -16,24 +16,27 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-  //  private static final String SECRET_KEY="c2VjcmV0b2RlbHJlc3RhdXJhbnRl";
-    private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
-    public String getToken(UserDetails user) {
+
+    @Value("${jwt.secret}")
+    private  String SECRET_KEY;
+    public String getToken(User user) {
         return getToken(new HashMap<>(),user);
     }
 
-    private  String getToken(HashMap<String,Object> extraClaims, UserDetails user) {
+    private  String getToken(HashMap<String,Object> extraClaims, User user) {
    return Jwts
            .builder()
-           .setClaims(extraClaims)
-           .setSubject(user.getUsername())
-           .setIssuedAt(new Date(System.currentTimeMillis()))
-           .setExpiration(new Date(System.currentTimeMillis()+100*60*24))
-           .signWith(getKey(), SignatureAlgorithm.HS256)
+           .claims(extraClaims)
+           .claim("userId",user.getId())
+           .claim("lasName",user.getLastname())
+           .subject(user.getUsername())
+           .issuedAt(new Date(System.currentTimeMillis()))
+           .expiration(new Date(System.currentTimeMillis()+100*60*24))
+           .signWith(getKey())
            .compact();
     }
 
-    private Key getKey(){
+    private SecretKey getKey(){
        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
        return Keys.hmacShaKeyFor(keyBytes);
 
@@ -51,11 +54,11 @@ public class JwtService {
     private Claims getAllClaims(String token)
     {
         return Jwts
-                .parserBuilder()
-                .setSigningKey(getKey())
+                .parser()
+                .verifyWith(getKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public <T> T getClaim(String token, Function<Claims,T> claimsResolver)
